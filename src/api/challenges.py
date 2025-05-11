0c7639842e62c97e3d72d79bc4fcb14dabca2f0f
ann4grillo <anna@grillo.net>
2025-05-11 15:23:43 -0700

    challenge submission

--- src/api/challenges.py
+++ src/api/challenges.py
@@ -19,7 +19,10 @@ class ChallengeCreateRequest(BaseModel):
     user_id: int
     title: str
     description: str
-    
+
+class SubmissionRequest(BaseModel):
+    user_id: int
+    tag_id: int
 
 @router.post("", status_code=status.HTTP_201_CREATED)
 def create_challenge(req: ChallengeCreateRequest):
@@ -44,3 +47,36 @@ def create_challenge(req: ChallengeCreateRequest):
             {"user_id": req.user_id,  "title": req.title, "description": req.description},
         ).one()
     return {"challenge_id": result[0], "message": "Challenge created successfully"}
+
+@router.post("/{challenge_id}/submission", status_code=status.HTTP_204_NO_CONTENT)
+def checkout(challenge_id: int, req: SubmissionRequest):
+    """
+    Sumbit a tag you've made to a challenge
+    """
+    with db.engine.begin() as connection:
+        challenge_exists = connection.execute(
+            sqlalchemy.text("SELECT 1 FROM challenges WHERE id = :challenge_id"),
+            {"challenge_id": challenge_id},
+        ).fetchone()
+        if not challenge_exists:
+            raise HTTPException(status_code=404, detail="Challenge not found")
+        
+        tag_exists = connection.execute(
+            sqlalchemy.text("SELECT 1 FROM user_tags WHERE id = :tag_id"),
+            {"tag_id": req.tag_id},
+        ).fetchone()
+        if not tag_exists:
+            raise HTTPException(status_code=404, detail="Tag not found")
+        
+        connection.execute(
+            sqlalchemy.text("""
+                INSERT INTO challenge_submissions (user_id, challenge_id, tag_submission)
+                VALUES (:user_id, :challenge_id, :tag_submission)
+            """),
+            {"user_id": req.user_id,  "challenge_id": challenge_id, "tag_submission": req.tag_id},
+        )
+
+    return 
+        
+
+
